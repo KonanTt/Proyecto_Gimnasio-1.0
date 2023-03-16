5f97d78617d0eed0c4c9f592aef54037abebd5d2
KonanTt <113714813+KonanTt@users.noreply.github.com>
2023-03-16 18:51:38 -0500

    Tatiana INSTRUCCTOR

--- src/modelo/Instructor.java
+++ src/modelo/Instructor.java
@@ -4,30 +4,33 @@ import java.util.Date;
 
 public class Instructor extends Persona {
 
+    public Instructor() {
+    }
+
     private int ins_codigo;
     private int ins_codper;
     private String ins_areatrabajo;
     private String ins_horario;
     private String ins_estado;
+    private double ins_suedo;
 
-    public Instructor() {
-    }
-
-    public Instructor(int ins_codigo, int ins_codper, String ins_areatrabajo, String ins_horario, String ins_estado) {
+    public Instructor(int ins_codigo, int ins_codper, String ins_areatrabajo, String ins_horario, String ins_estado, double ins_suedo) {
         this.ins_codigo = ins_codigo;
         this.ins_codper = ins_codper;
         this.ins_areatrabajo = ins_areatrabajo;
         this.ins_horario = ins_horario;
         this.ins_estado = ins_estado;
+        this.ins_suedo = ins_suedo;
     }
 
-    public Instructor(int ins_codigo, int ins_codper, String ins_areatrabajo, String ins_horario, String ins_estado, int per_codigo, String per_cedula, String per_nombre, String per_apellido, Date per_fechaNac, String per_telefono, String per_direccion) {
+    public Instructor(int ins_codigo, int ins_codper, String ins_areatrabajo, String ins_horario, String ins_estado, double ins_suedo, int per_codigo, String per_cedula, String per_nombre, String per_apellido, Date per_fechaNac, String per_telefono, String per_direccion) {
         super(per_codigo, per_cedula, per_nombre, per_apellido, per_fechaNac, per_telefono, per_direccion);
         this.ins_codigo = ins_codigo;
         this.ins_codper = ins_codper;
         this.ins_areatrabajo = ins_areatrabajo;
         this.ins_horario = ins_horario;
         this.ins_estado = ins_estado;
+        this.ins_suedo = ins_suedo;
     }
 
     public int getIns_codigo() {
@@ -69,4 +72,14 @@ public class Instructor extends Persona {
     public void setIns_estado(String ins_estado) {
         this.ins_estado = ins_estado;
     }
-}
+
+    public double getIns_suedo() {
+        return ins_suedo;
+    }
+
+    public void setIns_suedo(double ins_suedo) {
+        this.ins_suedo = ins_suedo;
+    }
+
+
+    }
